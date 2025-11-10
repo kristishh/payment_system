cc2ffd5d9e1eab13f3b1e4c0cc5ee33d13ff6f65
Kristiyan Tishev <tishev.kristyan@gmail.com>
2025-11-10 15:02:39 +0200

    react app initial commit

---
+++ client-side/src/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  // CRITICAL: This line tells Tailwind to scan the entire 'src' folder for classes
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
